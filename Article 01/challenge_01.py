95f7aa4baadc46a0836e248e542fa21255d46e26
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-07-16 13:53:06 +0530

    add: challenge_01.py & text_update.json

---
+++ Article 01/challenge_01.py
@@ -0,0 +1,29 @@
+from pyrogram import Client,types
+
+
+bot = Client(
+    name='examplebot',           # pass whatever name (bot name, your name, etc)
+    api_id=12345,                # pass your API_ID (as a int or str)
+    api_hash='your api hash',    # pass your API_HASH (as a str)
+    bot_token='your bot token'   # pass your BOT_TOKEN (as a str)
+)
+
+
+@bot.on_message()
+def echo(client,message:types.Message):
+    text = message.text
+    firstname = message.from_user.first_name
+    user_id = message.from_user.id
+    username = message.from_user.username
+    datetime = message.date
+    
+    custom_reply = (
+        f'**Name:** {firstname}\n',
+        f'**ID:** {user_id}\n',
+        f'**Username**: {username}\n',
+        f'**Text:** {text}\n',
+        f'**Date & Time:** {datetime}'
+    )
+    message.reply(custom_reply)
+
+bot.run()
