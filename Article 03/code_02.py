69feb0a6f208d061663acccfe690d10c2a65d495
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-07-20 13:28:12 +0530

    add second section code of the article 03

---
+++ Article 03/code_02.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters, types
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+bot = Client(
+    name='examplebot',           
+    api_id=12345,                
+    api_hash='your api hash',    
+    bot_token='your bot token'   
+)
+
+
+keyboard = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton('Yes😍',callback_data='yes'),
+            InlineKeyboardButton('No😏',callback_data='no')
+        ]
+    ]
+)
+
+
+@bot.on_message(filters.command('start') & filters.private)
+def love(client, message):
+    chat_id = message.chat.id
+    client.send_message(
+        chat_id=chat_id,
+        text='Do you like me?',
+        reply_markup=keyboard
+    )
+
+
+bot.run()
