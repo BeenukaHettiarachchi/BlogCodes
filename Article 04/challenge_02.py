e984fd8439e1c37435d161a044eac08baba83dfe
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-07-23 23:15:57 +0530

    add: challenge 02 answer code in article 04

---
+++ Article 04/challenge_02.py
@@ -0,0 +1,26 @@
+my_ex_list = [
+    "Zendaya",
+    "Emma Watson",
+    "Scarlett Johansson",
+    "Margot Robbie",
+    "Ana de Armas",
+    "Alexandra Daddario",
+    "Gal Gadot",
+    "Selena Gomez",
+    "Taylor Swift",
+    "Jennifer Lawrence",
+    "Sydney Sweeney",
+    "Megan Fox",
+    "Dua Lipa",
+    "Emilia Clarke",
+    "Florence Pugh",
+    "Millie Bobby Brown",
+    "Natalie Portman",
+    "Rihanna",
+    "Ariana Grande",
+    "Sabrina Carpenter"
+]
+
+crush_list = [celeb.split(' ')[0] for celeb in my_ex_list]
+
+print(crush_list)
