15a0869abcefdfd8fbbeb7ee5a00794a60c22bad
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-07-26 20:08:07 +0530

    add map() example code

--- Article 05/01.py
+++ Article 05/01.py
@@ -1,1 +1,22 @@
-# zip()
+# map()
+
+
+# Normal Method 
+players = ["emma Watson","Ana de Armas","TAYLOR SWIFT","jennifer lawrence"]
+
+players = [name.title() for name in players]
+print(players)
+
+# Iterator
+players = ["emma Watson","Ana de Armas","TAYLOR SWIFT","jennifer lawrence"]
+players = map(str.title, players)
+
+for i in players:
+    print(i)
+
+# Pro Method
+players = ["emma Watson","Ana de Armas","TAYLOR SWIFT","jennifer lawrence"]
+
+players = list(map(str.title, players))
+print(players)
+
