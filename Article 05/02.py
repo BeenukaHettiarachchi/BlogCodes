fe8cda07bf5ff8f4896dc539768162eb39b5e8e8
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-07-26 21:02:08 +0530

    add example code for zip()

--- Article 05/02.py
+++ Article 05/02.py
@@ -1,1 +1,23 @@
-# map()
+# zip()
+
+players = ["Emma Watson","Ana de Armas","Taylor Swift","Jennifer Lawrence"]
+roles = ["Midfielder","Defender","Goalkeeper","Striker"]
+
+# Normal Method
+lineup = []
+for i,player in enumerate(players):
+    lineup.append((player,roles[i]))
+print(lineup)
+
+# One liner Method
+lineup = [(player,roles[i]) for i,player in enumerate(players)]
+print(f'{lineup}')
+
+# Iterator
+lineup = zip(players, roles)
+for i in lineup:
+    print(i)
+
+# Pro Method
+lineup = list(zip(players, roles))
+print(lineup)
