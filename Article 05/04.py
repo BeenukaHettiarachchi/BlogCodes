fa22d917acce07e7745609432f2bad6b1a411ef8
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-07-26 23:08:13 +0530

    add example code for all()

--- Article 05/04.py
+++ Article 05/04.py
@@ -1,1 +1,9 @@
-# any()
+# all()
+
+
+attendance = [True,True,True,False]
+
+if all(attendance):
+    print('Full squad on the ground — game on!')
+else:
+    print('Heads up: Some players haven\'t shown up yet.')
