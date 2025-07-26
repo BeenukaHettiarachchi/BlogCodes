74e2f3267dfabffaa9b0d6654762916863f0ce81
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-07-26 23:10:01 +0530

    add challenge 02 code

---
+++ Article 05/challenge_02.py
@@ -0,0 +1,10 @@
+
+
+
+
+attendance = [True,True,True,False]
+
+if any(attendance):
+    print('Heads up: Some players haven\'t shown up yet.')
+else:
+    print('Full squad on the ground — game on!')
