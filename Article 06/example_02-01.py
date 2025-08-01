b217f21aee418e2695d4b5a16288a7f917a1ed1b
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-08-01 12:17:26 +0530

    add example 02 part 1 & 2 of article 06

---
+++ Article 06/example_02-01.py
@@ -0,0 +1,24 @@
+# Dog 1
+dog1_name = "Buddy"
+dog1_sound = "Woof"
+
+# Dog 2
+dog2_name = "Max"
+dog2_sound = "Bark"
+
+def speak(name,sound):
+    print(f"{name} says {sound}")
+
+def run(name):
+    print(f"{name} is running!")
+
+
+# Dog 01
+speak(dog1_name,dog1_sound)
+run(dog1_name)
+
+print()
+
+# Dog 02
+speak(dog2_name,dog2_sound)
+run(dog2_name)
---
+++ Article 06/example_02-02.py
@@ -0,0 +1,29 @@
+# Dog 1
+dog1_name = "Buddy"
+dog1_sound = "Woof"
+
+# Dog 2
+dog2_name = "Max"
+dog2_sound = "Bark"
+
+def dog1_speak():
+    print(f"{dog1_name} says {dog1_sound}")
+
+def dog1_run():
+    print(f"{dog1_name} is running!")
+
+
+def dog2_speak():
+    print(f"{dog2_name} says {dog2_sound}")
+
+def dog2_run():
+    print(f"{dog2_name} is running!")
+
+
+dog1_speak()
+dog1_run()
+
+print()
+
+dog2_speak()
+dog2_run()
