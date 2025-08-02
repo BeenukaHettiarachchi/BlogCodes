220d7fe84fc349d5808134e7bccc807e94339a93
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-08-02 16:38:02 +0530

    update: article 07 code

---
+++ Article 07/07.py
@@ -0,0 +1,11 @@
+class Dog:
+    
+    def __init__(self,name,breed,color,age):
+        self.name = name
+        self.breed = breed
+        self.color = color
+        self.age = age
+
+tommy = Dog('tommy','Labrador Retriever','Black',3)
+dora = Dog('dora','German Shepherd','Black & Tan',6)
+luca = Dog('luca','Doberman','Black & Rust',4)
