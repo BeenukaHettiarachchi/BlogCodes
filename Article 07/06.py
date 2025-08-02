895ad221c1ae3b0bf3506986f4bfb757c05d9f50
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-08-02 18:45:47 +0530

    update: article 07 code

--- Article 07/06.py
+++ Article 07/06.py
@@ -6,3 +6,5 @@ class Dog:
 tommy = Dog('tommy','Labrador Retriever','Black',3)
 dora = Dog('dora','German Shepherd','Black & Tan',6)
 luca = Dog('luca','Doberman','Black & Rust',4)
+
+print(tommy.name)
