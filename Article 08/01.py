e6c0384ffe2509b4c42218d0ca0f6f8b32442bd1
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-08-07 10:26:52 +0530

    add Article 08/01.py

---
+++ Article 08/01.py
@@ -0,0 +1,25 @@
+class Dog:
+    
+    def __init__(self,name,breed,color,age):
+        self.name = name
+        self.breed = breed
+        self.color = color
+        self.age = age
+        
+    def run(self):
+        print(self.name, 'is running.')
+        
+    def sleep(self):
+        print(self.name,'is sleeping.')
+        
+    def eat(self):
+        print(self.name, 'is eating.')
+
+
+tommy = Dog('tommy','Labrador Retriever','Black',3)
+dora = Dog('dora','German Shepherd','Black & Tan',6)
+luca = Dog('luca','Doberman','Black & Rust',4)
+
+tommy.eat()
+dora.sleep()
+luca.run()
