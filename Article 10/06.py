5276d5d6797b031eddd7f831996b7b1f7aeb0281
Beenuka <beenuka.hettiarachchi.net@gmail.com>
2025-08-21 21:48:53 +0530

    example 06 for article 10

--- Article 10/06.py
+++ Article 10/06.py
@@ -4,4 +4,14 @@ class Dog:
     def __init__(self,name,breed,age):
         self.name = name    
         self.breed = breed  
-        self.age = age      
+        self.age = age
+        Dog.dog_count += 1
+        
+
+tommy = Dog('tommy','Labrador Retriever',3)
+print(Dog.dog_count)
+dora = Dog('dora','German Shepherd',6)
+print(Dog.dog_count)
+luca = Dog('luca','Doberman',4)
+print(Dog.dog_count)
+
